be49621e08dd654df2f52f03e9e2c09035ccce64
Pradip <prad4787@gmail.com>
2022-07-22 17:45:57 +0545

    2fa auth added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('google2fa_secret')->nullable();
+            $table->boolean("2fa_verified")->default(false);
+
+            
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -1,1 +1,60 @@
-@extends("auth.layouts")
+@extends('auth.layouts')
+
+
+@section('content')
+    <div class="login-cover-wrapper">
+        <div class="card shadow-none">
+            <div class="card-body">
+                <div class="text-center">
+                    <h4>Sign In</h4>
+                    <p>Sign In to your account</p>
+                </div>
+                <form class="form-body row g-3" method="POST" action="{{ route('post-login') }}">
+                    @csrf
+                    <div class="col-12">
+                        <label for="inputEmail" class="form-label">Email</label>
+                        <input type="email"
+                            class="form-control {{ isset($errors) && $errors->has('email') ? 'is-invalid' : '' }}"
+                            id="inputEmail" name="email">
+                        @if (isset($errors) && $errors->has('email'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        @endif
+                    </div>
+                    <div class="col-12">
+                        <label for="inputPassword" class="form-label">Password</label>
+                        <input type="password"
+                            class="form-control  {{ isset($errors) && $errors->has('password') ? 'is-invalid' : '' }}"
+                            id="inputPassword" name="password">
+
+                        @if (isset($errors) && $errors->has('password'))
+                            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        @endif
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRemember"
+                                name="remember" value="true">
+                            <label class="form-check-label" for="flexSwitchCheckRemember">Remember
+                                Me</label>
+                            @if (isset($errors) && $errors->has('password'))
+                                <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-12">
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-dark">Sign In</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
