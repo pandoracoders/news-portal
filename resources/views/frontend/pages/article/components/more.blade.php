73e777e3b2539ba2e36545b1452dee959376264f
Sandip Dangal <sandipdangal77@gmail.com>
2022-08-22 12:25:03 +0545

    css change for more read section

--- resources/views/frontend/pages/article/components/more.blade.php
+++ resources/views/frontend/pages/article/components/more.blade.php
@@ -1,21 +1,26 @@
 <div class="row similar-post-container">
     @foreach ($article->more() as $article)
-        <div class="col-md-4 col-lg-3">
+        <div class="col-md-4 col-lg-3 post-section">
             <div class="similar-post">
                 <div class="image">
                     <figure class="m-0">
                         <a href="{{ route('singleArticle', $article->slug) }}">
-                            <img src="{{ asset($article->image) }}" alt="{{ $article->title }}" class="image_img img-fluid">
+                            <img src="{{ asset($article->image) }}" alt="{{ $article->title }}"
+                                class="image_img img-fluid">
                         </a>
                     </figure>
                 </div>
                 <div class="similar-post-title">
-                    <div class="title"><a href="{{ route('singleArticle', $article->slug) }}"> {{ $article->title }}</a>
+                    <div class="title">
+                        <a href="{{ route('singleArticle', $article->slug) }}"> {{ $article->title }}</a>
                     </div>
                     <div class="meta">
-                        <p class="article-date"> {{ dateFormat($article->published_at) }} | </p><p class="article-author"><a href="{{ route('authorArticle', $article->writer->slug) }}">
-                            {{ $article->writer->alias_name }} 
-                        </a></p>
+                        <p class="article-date"> {{ dateFormat($article->published_at) }} | </p>
+                        <p class="article-author">
+                            <a href="{{ route('authorArticle', $article->writer->slug) }}">
+                                {{ $article->writer->alias_name }}
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
